9f4c8ca0acf13e6d6dcf8e3a9a45c73344381b83
Felix <felix.m.schneider@gmx.de>
2022-11-09 11:53:47 +0100

    linted code

--- src/dummy_recommender.py
+++ src/dummy_recommender.py
@@ -1,5 +1,13 @@
+"""
+random selector
+"""
+
+import random
+
 def random_choice(df_movies, number=10):
-    import random
+    """
+    doing random selection from dataframe
+    """
     all_movies=list(df_movies["title"].unique())
     return random.choices(all_movies, k=number)
 
@@ -8,15 +16,13 @@ def random_choice(df_movies, number=10):
 if __name__ == "__main__":
     import pandas as pd
 
-    datapath="../data/"
+    DATAPATH="../data/"
 
 
-    df_movies=pd.read_csv(datapath+"movies.csv")
+    DF_MOVIES=pd.read_csv(DATAPATH+"movies.csv")
 
-    selection=random_choice(df_movies)
+    selection=random_choice(DF_MOVIES)
 
     print("the following movies have been selected for you:")
     for movie in selection:
         print(movie)
-
-    
